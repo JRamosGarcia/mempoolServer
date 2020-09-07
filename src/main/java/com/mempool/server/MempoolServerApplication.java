b250ba8fcc1f69d2daa0071d9ac62834a32490a0
jramos <juanramosgarcia@protonmail.com>
2020-09-07 07:49:04 +0200

    First commit

---
+++ src/main/java/com/mempool/server/MempoolServerApplication.java
@@ -0,0 +1,13 @@
+package com.mempool.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MempoolServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MempoolServerApplication.class, args);
+	}
+
+}
